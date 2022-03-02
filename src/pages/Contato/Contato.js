7aeb6fb7576236b27055e73500c25b42f7de6abd
malanski <malanskiwork@gmail.com>
2022-03-01 23:12:52 -0300

    add Contato page Content

--- src/pages/Contato/Contato.js
+++ src/pages/Contato/Contato.js
@@ -1,7 +1,26 @@
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import Facebook from '@mui/icons-material/Facebook';
+import Instagram from '@mui/icons-material/Instagram';
+
+
+
+
 export const Contato = (props) => {
     return (
         <div>
             <h1>Contatos</h1>
+            <main>
+                <div className="main-ul">
+                    <a href="https://www.linkedin.com/in/ulisses-malanski/"><LinkedInIcon /> Linkedin Profile</a><br/>
+                    <a href="https://www.facebook.com/ulisses.malanski"><Facebook /> Facebook Profile</a><br/>
+
+                    <a href="https://www.github.com/malanski"><GitHubIcon /> GitHub Repository's</a><br/>
+                    <a href='https://www.instagram.com/ulissesmalanski_tattoo/'><Instagram />Instagram Art and Tattoo's</a>
+                    
+
+                </div>
+            </main>
         </div>
 
     )
--- src/pages/Habilidades/Habilidades.js
+++ src/pages/Habilidades/Habilidades.js
@@ -2,6 +2,84 @@ export const Habilidades = (props) => {
     return (
         <div>
             <h1>Habilidades</h1>
+
+            <div className="flex-row">
+                <main>
+                    <h2 >Meus projetos utilizam:</h2>
+
+                    
+                    <ul className="main-ul">
+                        <li>
+                            <span>Front-end</span>
+                            <blockquote>
+                                HTML<br/>
+                                CSS - SASS<br/>
+                                JavaScript<br/>
+                                React<br/>
+                                Node<br/>
+                                VsCode<br/>
+                                GitHub<br/>
+                            </blockquote>
+                        </li>
+                        <li>
+                            <span>Design</span>
+                            <blockquote>
+                                Desenho técnico e artístico<br/>
+                                Pintura<br/>
+                                Ilustração<br/>
+                                Photoshop<br/>
+                                Illustrator<br/>
+                                Figma<br/>
+                                UXDesign<br/>
+                                Premiere
+                            </blockquote>
+                        </li>
+                        <li>
+                            <span>Audio</span>
+                            <blockquote>
+                                Ableton Live<br/>
+                                Composição musical<br/>
+                                Violão e Guitarra<br/>
+                                Mixagem<br/>
+                                Produção e edição
+                            </blockquote>
+                        </li>
+                    </ul>
+                </main>
+
+                <main className="ninja">
+                    <h2 >Uma dadiva dos Ninja</h2>
+
+                    
+                    <ul className="main-ul">
+                        <li>
+                            <span>Tai-Jutsu</span>
+                            <blockquote>
+                                Kungfu: Shaolin do Norte e Sul<br/>
+                                Bastão<br/>
+                                Luta corpo-a-corpo
+                            </blockquote>
+                        </li>
+                        <li>
+                            <span>Parkour</span>
+                            <blockquote>
+                                Subida de muros<br/>
+                                Rolamento evasivo<br/>
+                                Treinamento de Resistência<br/>
+                            </blockquote>
+                        </li>
+                        <li>
+                            <span>Arte da Furtividade</span>
+                            <blockquote>
+                                Taticas de Combate<br/>
+                                Camuflagem das Sombras<br/>
+
+
+                            </blockquote>
+                        </li>
+                    </ul>
+                </main>
+            </div>
         </div>
 
     )
